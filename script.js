2ed9d57302f6e0ad99e649d79bc65355f4abdee4
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-02-21 22:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function nextPalindrome(num) {
-  //your JS code here.
+  // Helper function to check if a number is a palindrome
+  function isPalindrome(number) {
+    return number.toString() === number.toString().split('').reverse().join('');
+  }
+
+  // Start looking for the next palindrome after the given number
+  let nextNum = num + 1;
+  while (!isPalindrome(nextNum)) {
+    nextNum++;
+  }
+  
+  return nextNum;
 }
 
-const input = prompt("Enter a palindrome number");
+// This part would be outside of the function in your actual code,
+// since prompt and alert can't be run in this environment.
+const input = parseInt(prompt("Enter a number to find the next palindrome."));
 alert(nextPalindrome(input));
